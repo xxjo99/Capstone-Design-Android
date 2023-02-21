62100df05a0702f8570740a42d613a0a11443621
KwangJo Ko <xxjo99@gmail.com>
2023-02-21 20:34:02 +0900

    design update

--- app/src/main/java/com/delivery/mydelivery/home/SearchResultStore.java
+++ app/src/main/java/com/delivery/mydelivery/home/SearchResultStore.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.delivery.mydelivery.R;
 import com.delivery.mydelivery.retrofit.RetrofitService;
@@ -19,6 +21,7 @@ import com.delivery.mydelivery.store.StoreApi;
 import com.delivery.mydelivery.store.StoreVO;
 
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -31,6 +34,8 @@ public class SearchResultStore extends Fragment {
     static RecyclerView storeRecyclerView;
     static SearchResultStoreAdapter searchResultStoreAdapter;
     static List<StoreVO> storeList;
+    static LinearLayout emptyLayout;
+    static NestedScrollView searchResultLayout;
 
     // view, context
     View view;
@@ -45,6 +50,10 @@ public class SearchResultStore extends Fragment {
         assert inflater != null;
         view = inflater.inflate(R.layout.fragment_home_search_result_store, container, false);
         context = getContext();
+
+        // 초기화
+        emptyLayout = view.findViewById(R.id.emptyLayout);
+        searchResultLayout = view.findViewById(R.id.searchResultLayout);
 
         // 리사이클러뷰 설정
         storeRecyclerView = view.findViewById(R.id.storeRecyclerView);
@@ -65,8 +74,17 @@ public class SearchResultStore extends Fragment {
                     @Override
                     public void onResponse(@NonNull Call<List<StoreVO>> call, @NonNull Response<List<StoreVO>> response) {
                         storeList = response.body();
-                        searchResultStoreAdapter = new SearchResultStoreAdapter(storeList, context);
-                        storeRecyclerView.setAdapter(searchResultStoreAdapter);
+
+                        if (Objects.requireNonNull(storeList).isEmpty()) {
+                            emptyLayout.setVisibility(View.VISIBLE);
+                            searchResultLayout.setVisibility(View.GONE);
+                        } else {
+                            emptyLayout.setVisibility(View.GONE);
+                            searchResultLayout.setVisibility(View.VISIBLE);
+
+                            searchResultStoreAdapter = new SearchResultStoreAdapter(storeList, context);
+                            storeRecyclerView.setAdapter(searchResultStoreAdapter);
+                        }
                     }
 
                     @Override
