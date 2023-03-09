420cabff2caa8723254722d778c0f90c15877e9f
SeJin4019 <ksg4019113@naver.com>
2023-03-09 12:14:56 +0900

    포인트 충전 버튼 위치 조절, 주문내역 , 마이페이지 공지사항 항목 추가, 다이얼로그(취소나 경고문구 빼고 버튼 민트로 변경) , 요청사항(일회용 용기 빼주세요 제거)

---
+++ app/src/main/java/com/delivery/mydelivery/order/order_history.java
@@ -0,0 +1,16 @@
+package com.delivery.mydelivery.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.delivery.mydelivery.R;
+
+public class order_history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_history);
+    }
+}
