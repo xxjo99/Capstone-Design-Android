0205cb137eeee39accd3cec523db91626a688049
SeJin4019 <ksg4019113@naver.com>
2023-03-20 22:47:43 +0900

    배달 확인 디자인(이미지 버튼으로 변경 및 디자인)

--- app/src/main/java/com/delivery/mydelivery/home/HomeActivity.java
+++ app/src/main/java/com/delivery/mydelivery/home/HomeActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -40,7 +41,7 @@ public class HomeActivity extends AppCompatActivity {
     TextView infoTV;
 
     // 배달 확인 버튼
-    Button checkDeliveryBtn;
+    ImageButton checkDeliveryBtn;
 
     @SuppressLint("NonConstantResourceId")
     @Override
